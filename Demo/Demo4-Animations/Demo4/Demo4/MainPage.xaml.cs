4fc72ece7b245245398860c7577b640d4e50f4f5
udaraxamariners <udara@xamariners.com>
2017-09-21 18:17:40 +0800

    demo 4

--- Demo/Demo4-Animations/Demo4/Demo4/MainPage.xaml.cs
+++ Demo/Demo4-Animations/Demo4/Demo4/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;   
+using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
 
@@ -15,9 +17,72 @@ namespace Demo4
             InitializeComponent();
         }
 
+        Stopwatch stopwatch = new Stopwatch();
+        bool pageIsActive;
+        float t;
+        const double cycleTime = 1000; // in milliseconds
+
+        private void InitAnimation()
+        {
+            pageIsActive = true;
+            stopwatch.Start();
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(33), () =>
+            {
+                // calculate t for current tick with regards to cycletime
+                t = (float)(stopwatch.Elapsed.TotalMilliseconds % cycleTime / cycleTime);
+                SkCanvasView.InvalidateSurface();
+
+                if (!pageIsActive)
+                {
+                    stopwatch.Stop();
+                }
+                return pageIsActive;
+            });
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            InitAnimation();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            pageIsActive = false;
+        }
+
         private void SkCanvasView_OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
+            // Init skcanvas
+            SKImageInfo skImageInfo = e.Info;
+            SKSurface skSurface = e.Surface;
+            SKCanvas skCanvas = skSurface.Canvas;
 
+            skCanvas.Clear(SKColors.White);
+
+            var skCanvasWidth = skImageInfo.Width;
+            var skCanvasheight = skImageInfo.Height;
+
+            // move canvas X,Y to center of screen
+            skCanvas.Translate((float)skCanvasWidth / 2, (float)skCanvasheight / 2);
+            // set the pixel scale of the canvas
+            skCanvas.Scale(skCanvasWidth / 200f);
+
+            // get Circle radius at this paint cycle
+            float radius = 70 * t;
+
+            // Drawing a Circle Stroke
+            using (SKPaint skPaint = new SKPaint())
+            {
+                skPaint.Style = SKPaintStyle.Stroke;
+                skPaint.IsAntialias = true;
+                skPaint.Color = SKColors.Red;
+                skPaint.StrokeWidth = 10;
+
+                skCanvas.DrawCircle(0, 0, radius, skPaint);
+            }
         }
     }
 }
